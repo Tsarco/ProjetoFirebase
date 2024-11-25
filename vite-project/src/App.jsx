5048a91e2f9f62d5b6711ede3f6011461d3889c4
Tsarco <wedontexist777@gmail.com>
2024-11-24 23:48:15 -0300

    funcionamento básico dos pacotes confirmado. Para rodar com dependencia, usa npm ci. Só info, básica, sem firebase ainda

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './navbar.jsx'
-import CollapsibleExample from './navbar.jsx'
+import Navbar from './navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <CollapsibleExample/>
+      <Navbar/>
     </>
   )
 }
--- vite-project/src/navbar.jsx
+++ vite-project/src/navbar.jsx
@@ -1,41 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-export default function CollapsibleExample() {
+export default function Navbar2() {
   return (
     <>
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                    Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                </NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-                </Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
         </Container>
-        </Navbar>
+      </Navbar>
     </>
   );
 }
-
