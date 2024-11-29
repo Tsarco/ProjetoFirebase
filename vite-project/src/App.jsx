1e28fe8516ed76395f2d7ef6c98399417a0620c0
Gabriel Dias <153240026+Tsarco@users.noreply.github.com>
2024-11-28 22:14:43 -0300

    Exibição básica dos dados
    CSS E integração firebase ainda necessaria

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,27 +1,16 @@
 import { useEffect, useState } from 'react'
-// import {AiOutlinePlus} from 'react-icons/ai'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './navbar.jsx'
 import Navbar from './navbar.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { db } from './firebase.js'
 import {query, collection, onSnapshot} from 'firebase/firestore'
 import Comida from './components/comida.jsx'
 import Tarefas from './components/Tarefas.jsx'
 
 function App() {
-  const [tarefas, setTarefas] = useState([])
-
-  const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[2F80ED] to-[#1CB5E0]`,
-  container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
-  heading: 'text-3xl font-bold text-center text-gray-800 p-2',
-  form: 'flex ustify-between', 
-  input: 'border p-2 w-full text-xl',
-  button: 'border p-4 ml-2 bg-purple-500 '
-};
+  const [tarefas, setTarefas] = useState(['Azul', 'Vermelho'])
 
   // //Ler dados do Firebase
   // useEffect(() => {
@@ -39,12 +28,12 @@ function App() {
 
   return (
     <>
-      <div className={style.bg}>
-        <div className={style.container}>
-        <h3 className={style.heading}> Aplicativo de Tarefas</h3>
-        <form className={style.form}>
-          <input className={style.input} type="text" placeholder="Adicionar Tarefa"/>
-          <button className={style.button}><AiOutlinePlus size={30} /></button>
+      <div className="background">
+        <div className="container">
+        <h3 className="header"> Aplicativo de Tarefas</h3>
+        <form className="form">
+          <input className="input" type="text" placeholder="Adicionar Tarefa"/>
+          <button className="button">Icone</button>
         </form>
         
         <ul>
@@ -54,7 +43,7 @@ function App() {
     
         
         </ul>
-        <p className={style.contagem}> Você tem 2 tarefas</p>
+        <p className="contagem"> Você tem 2 tarefas</p>
 
         </div>
       </div>
